645e1195a6cb4355597076b977a3491407e1e5f1
BRIAN L <bleemoon@yahoo.com>
2023-01-03 06:51:03 -0500

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,27 @@
+# sentence = 'The dog is named sammy'
+# print(sentence.capitalize())
+
+from tkinter import N
+
+
+first_name = 'Christopher'
+last_name = 'Harrison'
+
+output = 'Hello, '  + first_name + ' ' +last_name
+
+output = 'Hello, {} {} ' .format (first_name, last_name)
+print(output)
+
+firstName = input(Please type your First Name)
+lastName = input(Please type your Last Name)
+email_Add = input(Please type your Email Address)
+phoneNum = input(Please type your phone number)
+job_Title = input(Please type your Job Title)
+id_Number = input(Please type your id_Number)
+
+Print(lastName.upper(), +' '+ firstName.lower())
+print(job_title.capitalize())
+Print(id_Number)
+\n  
+print(email_Add.lower())
+print(phoneNum)
